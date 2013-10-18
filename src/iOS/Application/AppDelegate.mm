b0631867a7ce1a7e734ccf4b74e91ccb39fdcda5
Nick Donaldson <ndonald2@gmail.com>
2013-10-18 11:20:05 -0400

    Comments

--- src/iOS/Application/AppDelegate.mm
+++ src/iOS/Application/AppDelegate.mm
@@ -16,6 +16,8 @@
 {
     [super applicationDidFinishLaunching:application];
     
+    // Change this to a different root view controller if you like, then add an oF-app-backed view
+    // or view controller somewhere else in your app's hierarchy.
     [self.window setRootViewController:[[ofxiOSViewController alloc] initWithFrame:[[UIScreen mainScreen] bounds] app:(new testApp())]];
     
     return YES;
